d3521605a99261bbde685446cb0b0274d3ad4530
SabrinaBarros <sabrina.barros2045@gmail.com>
2023-12-10 06:01:22 -0300

    feat: countdown timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
-const App = () => {
-  return ''
-}
+import React from "react"
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {time: 5};
+  };
+
+  componentDidMount() {
+    this._startTimer();
+  };
+
+  _startTimer() {
+    const interval = setInterval(() => {
+
+      !this.state.time ? clearInterval(interval) : this.setState({time: this.state.time - 1})
+
+    }, 1000);
+  };
+
+  _minutesMask(seconds) {
+    return Math.floor(seconds / 60);
+  };
+
+  _secondsMask(seconds) {
+    return Math.floor(seconds % 60);
+  };
+
+  render() {
+    return (
+      <>
+        <span>
+          {this._minutesMask(this.state.time)}:{this._secondsMask(this.state.time)}
+        </span>
+        <button onClick={() => this.setState({time: 1500})}>Refresh</button>
+      </>
+  )}
+};
 
 export default App
