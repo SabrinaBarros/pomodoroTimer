155216fda581191f698ae3b8b5976003433c78c6
SabrinaBarros <sabrina.barros2045@gmail.com>
2024-01-17 00:32:00 -0300

    test: update timer format

--- cypress/e2e/pomodoro.cy.js
+++ cypress/e2e/pomodoro.cy.js
@@ -10,11 +10,11 @@ describe('Pomodoro', () => {
     cy.get('[data-test="refresh-btn"]').should('exist')
     cy.get('[data-test="toggle-btn"]').should('exist')
     
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
 
     cy.tick(5000)
 
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
 
     cy.get('[data-test="counter"]').should('have.text', '#0')
   })
@@ -59,18 +59,18 @@ describe('Timer Mode Buttons', () => {
     cy.clock()
 
     cy.tick(2000)
-    cy.get('[data-test="timer"]').should('have.text', '4:58')
+    cy.get('[data-test="timer"]').should('have.text', '04:58')
 
     focus_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
     cy.tick(2000)
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
 
     toggle_btn.click()
     cy.get('[data-test="timer"]').should('have.text', '24:56')
   })
 
-  it('Switches to Short Break Mode', () => {
+  it('Switches to Break Mode', () => {
     cy.visit('/')
 
     const toggle_btn = cy.get('[data-test="toggle-btn"]')
@@ -83,15 +83,15 @@ describe('Timer Mode Buttons', () => {
     cy.get('[data-test="timer"]').should('have.text', '24:58')
 
     short_break_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '5:0')
+    cy.get('[data-test="timer"]').should('have.text', '05:00')
     cy.tick(2000)
-    cy.get('[data-test="timer"]').should('have.text', '5:0')
+    cy.get('[data-test="timer"]').should('have.text', '05:00')
 
     toggle_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '4:58')
+    cy.get('[data-test="timer"]').should('have.text', '04:58')
   })
 
-  it('Switches to Long Break Mode', () => {
+  it('Switches to Interval Mode', () => {
     cy.visit('/')
 
     const toggle_btn = cy.get('[data-test="toggle-btn"]')
@@ -104,9 +104,9 @@ describe('Timer Mode Buttons', () => {
     cy.get('[data-test="timer"]').should('have.text', '24:58')
 
     long_break_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '15:0')
+    cy.get('[data-test="timer"]').should('have.text', '15:00')
     cy.tick(2000)
-    cy.get('[data-test="timer"]').should('have.text', '15:0')
+    cy.get('[data-test="timer"]').should('have.text', '15:00')
 
     toggle_btn.click()
     cy.get('[data-test="timer"]').should('have.text', '14:57')
@@ -129,13 +129,13 @@ describe('Refresh Button', () => {
     cy.get('[data-test="timer"]').should('have.text', '24:57')
 
     refresh_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
 
     cy.tick(4000)
-    cy.get('[data-test="timer"]').should('have.text', '25:0')
+    cy.get('[data-test="timer"]').should('have.text', '25:00')
   })
 
-  it('Refresh: Short Break Mode', () => {
+  it('Refresh: Break Mode', () => {
     cy.visit('/')
 
     const toggle_btn = cy.get('[data-test="toggle-btn"]')
@@ -147,16 +147,16 @@ describe('Refresh Button', () => {
     cy.clock()
 
     cy.tick(4000)
-    cy.get('[data-test="timer"]').should('have.text', '4:56')
+    cy.get('[data-test="timer"]').should('have.text', '04:56')
 
     refresh_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '5:0')
+    cy.get('[data-test="timer"]').should('have.text', '05:00')
 
     cy.tick(4000)
-    cy.get('[data-test="timer"]').should('have.text', '5:0')
+    cy.get('[data-test="timer"]').should('have.text', '05:00')
   })
 
-  it('Refresh: Long Break Mode', () => {
+  it('Refresh: Interval Mode', () => {
     cy.visit('/')
 
     const toggle_btn = cy.get('[data-test="toggle-btn"]')
@@ -171,10 +171,10 @@ describe('Refresh Button', () => {
     cy.get('[data-test="timer"]').should('have.text', '14:58')
 
     refresh_btn.click()
-    cy.get('[data-test="timer"]').should('have.text', '15:0')
+    cy.get('[data-test="timer"]').should('have.text', '15:00')
 
     cy.tick(4000)
-    cy.get('[data-test="timer"]').should('have.text', '15:0')
+    cy.get('[data-test="timer"]').should('have.text', '15:00')
   })
 
 })
